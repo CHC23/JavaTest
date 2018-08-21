bad723009e7094020405c08df95639c7b0600a90
chc <chc970223@126.com>
2018-08-21 18:59:29 +0800

    learn java

---
+++ Test/ExtendsTest/ExtendsTest.java
@@ -0,0 +1,45 @@
+class Animal{
+	private String name;
+	private int id;
+	public Animal(String name,int id){
+		//name=
+		this.name=name;
+		this.id=id;
+	}
+	
+	public void eat(){
+		System.out.println("进食");
+	}
+	
+	public void sleep(){
+		System.out.println("睡觉");
+	}
+	
+	public void introduction(){
+		System.out.println("我是" +id+"号"+name);
+	}
+}
+
+class Penguin extends Animal{
+	public Penguin(String name,int id){
+		super(name,id);
+	}
+}
+
+class Mouse extends Animal{
+	public Mouse(String name,int id){
+		super(name,id);
+	}
+}
+
+
+public class ExtendsTest{
+	public static void main(String[] args){
+		Animal animal_1=new Animal("penguin",001);
+		Animal animal_2=new Animal("mouse",002);
+		//animal_1;
+		animal_1.introduction();
+		animal_2.introduction();
+	}
+
+}
