315399d07e9676deec3c9ed0badd73b9de68a917
chc <chc970223@126.com>
2018-08-22 12:07:26 +0800

    learn java

---
+++ Test/EncapsulationTest/EncapsulationTest.java
@@ -0,0 +1,44 @@
+//java封装Encapsulation
+class Encapsulation{
+	private String name;
+	private String id;
+	private int age;
+	
+	
+	//以下public方法是外部类访问Encapsulation类成员变量的入口。
+	public int getAge(){
+		return age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getId(){
+		return id;
+	}
+	
+	public void setAge(int newAge){
+		age=newAge;
+	}
+	
+	public void setName(String newName){
+		name=newName;
+	}
+	
+	public void setId(String newId){
+		id=newId;
+	}
+}
+
+public class EncapsulationTest{
+	public static void main(String[] args){
+		Encapsulation encapsulation=new Encapsulation();
+		
+		encapsulation.setName("陈焕彩");
+		encapsulation.setAge(21);
+		encapsulation.setId("100001");
+		
+		System.out.println("姓名："+encapsulation.getName()+"年龄："+encapsulation.getAge()+"学号："+encapsulation.getId());
+	}
+}
