0a2bbde474f8a3f425f047e86c26762021713430
chc <chc970223@126.com>
2018-08-27 12:13:41 +0800

    learn java

--- Swing/src/game/FirstGameTest.java
+++ Swing/src/game/FirstGameTest.java
@@ -5,11 +5,30 @@ import java.awt.*;
 
 public class FirstGameTest extends JFrame{
 	
+	Image back=GameUtil.getImage("images/back.jpg");
+	Image plane=GameUtil.getImage("images/2.png");
+	int x=200,y=450;
+	
 	public void paint(Graphics g) {
-		g.drawLine(300, 300, 400, 600);
+		g.drawImage(back,0,0,null);
+		g.drawImage(plane,x,y,null);
+		x=x+10;
 	}
 	
-	
+	class PaintThread extends Thread{
+		
+		@Override
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(40);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
 	public void startFrame() {
 		this.setTitle("一个游戏");
@@ -18,6 +37,8 @@ public class FirstGameTest extends JFrame{
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		//this.addWinodwListener
 		this.setVisible(true);
+		
+		new PaintThread().run();			//启动重画窗口的线程
 	}
 	
 	public static void main(String[] args) {
