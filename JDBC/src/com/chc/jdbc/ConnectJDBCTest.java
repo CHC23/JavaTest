ef78445ac51f7772df9ffb42649a8ab621a28ae2
chc <chc970223@126.com>
2018-11-19 20:54:33 +0800

    learn java

---
+++ JDBC/src/com/chc/jdbc/ConnectJDBCTest.java
@@ -0,0 +1,27 @@
+package com.chc.jdbc;
+//jdbc数据库连接测试
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectJDBCTest {
+	public static void main(String[] args) {
+	
+		try {
+			//加载驱动类
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			long start=System.currentTimeMillis();
+			//建立连接
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/myblog?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT", "root", "12zx13zc");
+			long end=System.currentTimeMillis();
+			System.out.println(con);
+			System.out.println("建立连接耗时："+(end-start)+"毫秒");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
